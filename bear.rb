0284217c812d166d658466a2b7a14814babc3810
David Eastcroft <davideastcroft95@gmail.com>
2020-03-17 20:15:44 +0000

    Adds test and function for hungry bear.

--- bear.rb
+++ bear.rb
@@ -5,6 +5,13 @@ class Bear
   def initialize(name, type)
     @name = name
     @type = type
+    @stomach = []
   end
+
+  def bear_hungry()
+    return @stomach.size()
+  end
+
+  
 
 end
--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -3,11 +3,15 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../bear.rb')
+require_relative('../river.rb')
+require_relative('../fish.rb')
 
 class BearTest < MiniTest::Test
 
   def setup()
     @bear = Bear.new("Yogi", "Grizzly")
+    @river = River.new("Amazon", @fish)
+    @fish = Fish.new("Salmon", 20)
   end
 
   def test_bear_name
@@ -17,5 +21,14 @@ class BearTest < MiniTest::Test
   def test_bear_type
     assert_equal("Grizzly", @bear.type)
   end
+
+  def test_bear_hungry
+    assert_equal(0, @bear.bear_hungry)
+  end
+
+  # def test_eat_fish
+  #   @bear.eat_fish("Salmon")
+  #   assert_equal(1, @bear.stomach.size)
+  # end
 
 end
